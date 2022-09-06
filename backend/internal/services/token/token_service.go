3864a90d2464e7b4964abd0b802f69f05e35175f
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-09-06 10:46:30 +0430

    ✨ style: removed extra prints from GenerateJWT function

--- backend/internal/services/token/token_service.go
+++ backend/internal/services/token/token_service.go
@@ -1,7 +1,6 @@
 package tokenService
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/alireza/api/internal/contract"
@@ -15,7 +14,6 @@ type TokenService struct{}
 var service = &TokenService{}
 
 func (t *TokenService) GenerateJWT(user *models.User, expireAfter int64) (tokenString string, err error) {
-	fmt.Println(expireAfter)
 	expirationTime := time.Now().Add(time.Duration(expireAfter) * time.Second)
 	claims := &contract.Claims{
 		User: user.UserDetails,
@@ -23,8 +21,6 @@ func (t *TokenService) GenerateJWT(user *models.User, expireAfter int64) (tokenS
 			ExpiresAt: expirationTime.Unix(),
 		},
 	}
-	fmt.Println(time.Now())
-	fmt.Println(expirationTime)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err = token.SignedString(g.CFG.SecretKey)
 	return
