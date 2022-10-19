98fe02de3e10039976c0422ca2a4a01f08382a49
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-10-19 16:34:14 +0330

    🔧 refactor: used memoization on squares

--- frontend/src/components/UI/Squares.tsx
+++ frontend/src/components/UI/Squares.tsx
@@ -1,25 +1,32 @@
 // =============== Styles =============== //
 import styles from "@/styles/UI/Squares.module.css";
+import { useMemo } from "react";
 
 interface SquaresProps extends React.PropsWithChildren {
 	addSquares: boolean;
 }
+
+const LittleSquares: React.FC = () =>
+	useMemo(
+		() => (
+			<>
+				<div className={styles.square}></div>
+				<div className={styles.square}></div>
+				<div className={styles.square}></div>
+				<div className={styles.square}></div>
+				<div className={styles.square}></div>
+				<div className={styles.square}></div>
+			</>
+		),
+		[]
+	);
 
 const Squares: React.FC<SquaresProps> = (props: SquaresProps) => {
 	return (
 		<>
 			{props.addSquares && (
 				<div className={styles["square-container"]}>
-					{props.addSquares && (
-						<>
-							<div className={styles.square}></div>
-							<div className={styles.square}></div>
-							<div className={styles.square}></div>
-							<div className={styles.square}></div>
-							<div className={styles.square}></div>
-							<div className={styles.square}></div>
-						</>
-					)}
+					{props.addSquares && <LittleSquares />}
 					{props.children}
 				</div>
 			)}
