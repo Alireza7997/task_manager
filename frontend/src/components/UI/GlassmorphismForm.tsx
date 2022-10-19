379aacaed9456e729392714846ab64a20effcbdf
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-10-19 16:35:00 +0330

    🔧 refactor: GlassmorphismForm all style is pure tailwind from now on

--- frontend/src/components/UI/GlassmorphismForm.tsx
+++ frontend/src/components/UI/GlassmorphismForm.tsx
@@ -1,3 +1,6 @@
+// =============== Styles =============== //
+import styles from "@/styles/UI/GlassmorphismForm.module.css";
+
 // =============== Components =============== //
 import Squares from "./Squares";
 
@@ -9,17 +12,9 @@ interface GlassmorphismFormProps extends React.PropsWithChildren {
 const GlassmorphismForm: React.FC<GlassmorphismFormProps> = (
 	props: GlassmorphismFormProps
 ) => {
-	const border = "border-2 border-solid border-[#08071099]";
-	const style = "backdrop-blur-md rounded-xl w-[400px] bg-[#ffffff21] z-0";
-	const center = "absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2";
-	const centerItems = "flex flex-col justify-center space-y-8";
-	const mediaQuery = "xsMax:w-screen xsMax:h-screen";
 	return (
 		<Squares addSquares={props.addSquares}>
-			<form
-				className={`${centerItems} ${center} ${style} ${border} ${mediaQuery}`}
-				onSubmit={props.onSubmit}
-			>
+			<form className={styles["glassmorphism-form"]} onSubmit={props.onSubmit}>
 				{props.children}
 			</form>
 		</Squares>
