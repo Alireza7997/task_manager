aa117e0982d2afccf9126a86710cf8d352ff211b
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-10-20 18:42:14 +0330

    ✨ style: del button style added

--- frontend/src/components/TaskManager/Task.tsx
+++ frontend/src/components/TaskManager/Task.tsx
@@ -27,12 +27,14 @@ const Task: React.FC<TaskProps> = (props: TaskProps) => {
 		<div className={styles["task-row"]}>
 			<h5 className={styles["task-title"]}>{props.name}</h5>
 			<p className={styles["task-description"]}>{props.description}</p>
-			<RiDeleteBin2Line
-				className="cursor-pointer w-5 h-5"
+			<button
 				onClick={() => {
 					delete_task(globals.backend, props.id, props.setTasks);
 				}}
-			/>
+				className="p-[2px] rounded-sm bg-[#ff4d4d] hover:scale-[110%] transition-all"
+			>
+				<RiDeleteBin2Line className="w-5 h-5" color="#111111" />
+			</button>
 		</div>
 	);
 };
