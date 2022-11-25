bf436e4e6bf4fe24c0a56fbe529d2bc9d912c61b
Alireza <alirezajanbaz69@gmail.com>
2022-11-25 13:32:54 +0330

    fix(backend-task_manager): cascade on delete added

--- backend/migrations/3.user_sess_fk.sql
+++ backend/migrations/3.user_sess_fk.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE sessions 
-ADD CONSTRAINT fk_session_id FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT fk_session_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
 -- +migrate Down
 ALTER TABLE sessions
 DROP CONSTRAINT fk_session_id;
--- backend/migrations/8.task_manager_fk.sql
+++ backend/migrations/8.task_manager_fk.sql
@@ -1,12 +1,12 @@
 -- +migrate Up
 ALTER TABLE projects 
-ADD CONSTRAINT fk_projects FOREIGN KEY (user_id) REFERENCES users (id);
+ADD CONSTRAINT fk_projects FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
 
 ALTER TABLE tables 
-ADD CONSTRAINT fk_tables FOREIGN KEY (project_id) REFERENCES projects (id);
+ADD CONSTRAINT fk_tables FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE;
 
 ALTER TABLE tasks 
-ADD CONSTRAINT fk_tasks FOREIGN KEY (table_id) REFERENCES tables (id);
+ADD CONSTRAINT fk_tasks FOREIGN KEY (table_id) REFERENCES tables (id) ON DELETE CASCADE;
 -- +migrate Down
 ALTER TABLE projects
 DROP CONSTRAINT fk_projects;
