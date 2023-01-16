537a0149209be7142c2ffadd93e5510f56260f47
Mahmood Heidari <mahmoodh1378@gmail.com>
2023-01-16 13:22:34 +0330

    🎉 feat: due_date, start_date and finish_date of task are in UI

--- frontend/src/components/TaskManager/Task.tsx
+++ frontend/src/components/TaskManager/Task.tsx
@@ -11,6 +11,10 @@ import { Draggable } from "react-beautiful-dnd";
 import EditIcon from "@mui/icons-material/Edit";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import TimerIcon from "@mui/icons-material/Timer";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import { parseISO } from "date-fns";
 
 // =============== Components =============== //
 import { InputGlassmorphismFormProps } from "../UI/InputGlassmorphismForm";
@@ -22,6 +26,7 @@ import { action, TableData, TaskData } from "@/types/task_manager";
 // =============== API =============== //
 import useDeleteTask from "@/api/use_delete_task";
 import usePutTask from "@/api/use_put_task";
+import { Container } from "@mui/material";
 
 interface TaskIconProps extends React.PropsWithChildren {
 	onClick?: () => void;
@@ -32,7 +37,7 @@ const TaskIcon = (props: TaskIconProps) => {
 	return (
 		<button
 			onClick={props.onClick}
-			className={`${styles["button-icon"]} ${props.className} first:ml-0 ml-1 taskButton`}
+			className={`${styles["button-icon"]} ${props.className}`}
 		>
 			{props.children}
 		</button>
@@ -161,6 +166,14 @@ const Task: React.FC<TaskProps> = (props: TaskProps) => {
 		},
 	];
 
+	const hours: number | null = props.task?.due_date
+		? parseISO(
+				(Date.now() - parseISO(props.task?.due_date).getDate()).toString()
+		  ).getHours()
+		: null;
+	const daysRemaining = hours && hours / 24;
+	const hoursRemaining = hours && hours % 24;
+
 	return (
 		<>
 			{showEditPopup && (
@@ -204,40 +217,87 @@ const Task: React.FC<TaskProps> = (props: TaskProps) => {
 							>
 								{props.task.name}
 							</h5>
-							<TaskIcon
-								onClick={() => {
-									setShowDeletePopup(true);
-								}}
-								className="bg-[#c04d4d]"
-							>
-								<DeleteForeverIcon className="w-5 h-5" htmlColor="#e6e6e6" />
-							</TaskIcon>
-							<TaskIcon
-								onClick={() => {
-									setShowEditPopup(true);
-									setTaskFields(props.task);
-								}}
-								className="bg-[#4C70FF]"
-							>
-								<EditIcon className="w-5 h-5" htmlColor="#e6e6e6" />
-							</TaskIcon>
-							<TaskIcon
-								onClick={() => {
-									taskPut.mutate({ ...props.task, done: !props.task.done });
-								}}
-								className={`${
-									props.task.done ? "bg-[#1b721b]" : "bg-[#616161]"
-								}`}
-							>
-								{props.task.done ? (
-									<CheckBoxIcon className="w-5 h-5" htmlColor="#e6e6e6" />
-								) : (
-									<CheckBoxOutlineBlankIcon
-										className="w-5 h-5"
-										htmlColor="#e6e6e6"
-									/>
-								)}
-							</TaskIcon>
+							{/* Icons */}
+							<div className="space-y-1">
+								<div className="flex flex-row-reverse space-x-reverse space-x-1">
+									{/* Delete */}
+									<TaskIcon
+										onClick={() => {
+											setShowEditPopup(true);
+											setTaskFields(props.task);
+										}}
+										className="bg-[#4C70FF]"
+									>
+										<EditIcon className="w-5 h-5" htmlColor="#e6e6e6" />
+									</TaskIcon>
+									{/* Edit */}
+									<TaskIcon
+										onClick={() => {
+											setShowDeletePopup(true);
+										}}
+										className="bg-[#c04d4d]"
+									>
+										<DeleteForeverIcon
+											className="w-5 h-5"
+											htmlColor="#e6e6e6"
+										/>
+									</TaskIcon>
+								</div>
+								<div className="flex flex-row-reverse space-x-reverse space-x-1">
+									{/* Done */}
+									<TaskIcon
+										onClick={() => {
+											taskPut.mutate({ ...props.task, done: !props.task.done });
+										}}
+										className={`${
+											props.task.done ? "bg-[#1b721b]" : "bg-[#616161]"
+										}`}
+									>
+										{props.task.done ? (
+											<CheckBoxIcon className="w-5 h-5" htmlColor="#e6e6e6" />
+										) : (
+											<CheckBoxOutlineBlankIcon
+												className="w-5 h-5"
+												htmlColor="#e6e6e6"
+											/>
+										)}
+									</TaskIcon>
+									{/* Timer */}
+									<Tooltip
+										title={
+											(daysRemaining !== null &&
+												hoursRemaining !== null &&
+												((daysRemaining > 0 &&
+													hoursRemaining > 0 &&
+													`${daysRemaining} days and ${hoursRemaining} hours remaining`) ||
+													(daysRemaining <= 0 &&
+														hoursRemaining <= 0 &&
+														`${daysRemaining} days and ${hoursRemaining} hours passed`))) ||
+											"Error"
+										}
+										placement="bottom"
+									>
+										<Container className="!p-0">
+											<TaskIcon
+												className={`${
+													props.task.done
+														? "bg-[#1b721b]"
+														: daysRemaining !== null && hoursRemaining !== null
+														? daysRemaining! > 0 && hoursRemaining! > 0
+															? "bg-[#72691b]"
+															: daysRemaining! === 0 && hoursRemaining! > 0
+															? "bg-[#723e1b]"
+															: "bg-[#721b1b]"
+														: "bg-[#616161]"
+												}`}
+											>
+												<TimerIcon className="w-5 h-5" htmlColor="#e6e6e6" />
+											</TaskIcon>
+										</Container>
+									</Tooltip>
+								</div>
+							</div>
+							{/* End of Icons */}
 						</div>
 						<p
 							className={`${styles["task-description"]} ${
